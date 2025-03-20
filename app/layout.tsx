09adad680b9eb411dada4413859c2d321f431124
Coriano Harris <corianoharrispro@gmail.com>
2025-03-20 13:58:02 -0500

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Emotional Connect',
+  description: 'online dating by using advanced emotion AI to help people form deeper, more meaningful connections',
 }
 
 export default function RootLayout({
